6119cca30d490fef325e6a8f43c6abe203b1b623
Maria Jesus Canoles <maria.canoles.r@usach.cl>
2020-09-14 22:16:44 -0300

    probando los comandos para generar interfaz

---
+++ lab4/src/lab4/Lab4.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author mariajesuscanoles
+ */
+public class Lab4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        miMarco marco1 = new miMarco();
+        marco1.setVisible(true);
+        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
+
+class miMarco extends JFrame{
+    
+    public miMarco(){
+        //setSize(500,300);
+        //setLocation(500,300);
+        setBounds(500,300,550,250);
+        //setResizable(false);
+        //setExtendedState(Frame.MAXIMIZED_BOTH);
+        setTitle("Mi git");
+    }
+}
